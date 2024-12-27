39b7b7eb5939f4f3157ff02e7cd438a355aaa727
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-12-27 16:15:14 -0300

    otel: experimenting with receivertest
    Use the receivertest.CheckConsumeContract to test filebeatreceiver. The api
    does not quite work right now because it wants to look for a unique log id
    in the attributes, which we don't use for otelconsumer. I'm working on
    extending the api to support a callback function that returns the unique id
    from a user defined function, this would allow us to use a body field
    (`at metadata._id`).

--- x-pack/filebeat/fbreceiver/receiver_test.go
+++ x-pack/filebeat/fbreceiver/receiver_test.go
@@ -7,13 +7,23 @@ package fbreceiver
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/receivertest"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -60,9 +70,9 @@ func TestNewReceiver(t *testing.T) {
 	})
 	assert.NoError(t, err, "Error creating log consumer")
 
-	r, err := createReceiver(context.Background(), receiverSettings, &config, logConsumer)
-	assert.NoErrorf(t, err, "Error creating receiver. Logs:\n %s", zapLogs.String())
-	err = r.Start(context.Background(), nil)
+	r, err := NewFactory().CreateLogsReceiver(context.Background(), receiverSettings, &config, logConsumer)
+	assert.NoError(t, err, "Error creating log receiver")
+	err = r.Start(context.Background(), componenttest.NewNopHost())
 	assert.NoError(t, err, "Error starting filebeatreceiver")
 
 	ch := make(chan bool, 1)
@@ -88,4 +98,109 @@ func TestNewReceiver(t *testing.T) {
 found:
 	err = r.Shutdown(context.Background())
 	assert.NoError(t, err, "Error shutting down filebeatreceiver")
+}
+
+type logGenerator struct {
+	t           *testing.T
+	tmpDir      string
+	f           *os.File
+	filePattern string
+	sequenceNum int64
+}
+
+func newLogGenerator(t *testing.T, tmpDir string) *logGenerator {
+	f, err := os.CreateTemp(tmpDir, "input-*.log")
+	require.NoError(t, err)
+	return &logGenerator{
+		t:           t,
+		f:           f,
+		filePattern: "input-*.log",
+	}
+}
+
+func (g *logGenerator) Start() {
+	g.sequenceNum = 0
+}
+
+func (g *logGenerator) Stop() {}
+
+func (g *logGenerator) Generate() []receivertest.UniqueIDAttrVal {
+	// Generate may be called concurrently.
+	id := receivertest.UniqueIDAttrVal(strconv.FormatInt(atomic.AddInt64(&g.sequenceNum, 1), 10))
+
+	_, err := io.WriteString(g.f, fmt.Sprintf(`{"id": "%s", "message": "log message"}`, id))
+	require.NoError(g.t, err, "failed to write log line to file")
+	_, err = g.f.Write([]byte("\n"))
+	require.NoError(g.t, err, "failed to write newline to file")
+	require.NoError(g.t, g.f.Sync(), "failed to sync log file")
+
+	// And return the ids for bookkeeping by the test.
+	return []receivertest.UniqueIDAttrVal{id}
+}
+
+func TestConsumeContract(t *testing.T) {
+	tmpDir := t.TempDir()
+	const logsPerTest = 1
+
+	gen := newLogGenerator(t, tmpDir)
+	inputFile := gen.f.Name()
+
+	cfg := &Config{
+		Beatconfig: map[string]interface{}{
+			"filebeat": map[string]interface{}{
+				"inputs": []map[string]interface{}{
+					{
+						"type":    "filestream",
+						"id":      "filestream-test",
+						"enabled": true,
+						"paths": []string{
+							inputFile,
+						},
+						"queue.mem.flush.timeout": "0s",
+						"file_identity.native":    map[string]interface{}{},
+						"prospector": map[string]interface{}{
+							"scanner": map[string]interface{}{
+								"fingerprint.enabled": false,
+							},
+						},
+						"parsers": []map[string]interface{}{
+							{
+								"ndjson": map[string]interface{}{
+									"document_id": "id",
+								},
+							},
+						},
+					},
+				},
+			},
+			"output": map[string]interface{}{
+				"otelconsumer": map[string]interface{}{},
+			},
+			"logging": map[string]interface{}{
+				"level": "debug",
+				"selectors": []string{
+					"*",
+				},
+			},
+			"path.home": tmpDir,
+		},
+	}
+
+	// Run the contract checker. This will trigger test failures if any problems are found.
+	receivertest.CheckConsumeContract(receivertest.CheckConsumeContractParams{
+		T:        t,
+		Factory:  NewFactory(),
+		DataType: component.DataTypeLogs,
+		GetLogRecordID: func(record plog.LogRecord) (pcommon.Value, bool) {
+			body, exists := record.Body().Map().Get("@metadata")
+			if !exists {
+				return pcommon.NewValueEmpty(), exists
+			}
+
+			return body.Map().Get("_id")
+		},
+		Config:        cfg,
+		Generator:     gen,
+		GenerateCount: logsPerTest,
+	})
 }
